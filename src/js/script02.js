12b68a5da6b80b5f99cf8ca5fa32a950c075f8a0
Mauricio Antonio Chuahy Ferraz Silva <mauricio.ferrazs@outlook.com>
2024-12-04 09:27:54 -0300

    sintaxe

---
+++ src/js/script02.js
@@ -0,0 +1,7 @@
+//Exibe mensagem pelo alert
+alert("Mensagem enviada pelo alert através do arquivo externo");
+
+/*
+ *    Exibe mensagem pelo console
+ */
+console.log("Mensagem enviada pelo console através do arquivo externo");
