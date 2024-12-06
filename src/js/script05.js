9b8fe6b91b0d2c2bced6c3a52dab5b72a61a5452
Mauricio Antonio Chuahy Ferraz Silva <mauricio.ferrazs@outlook.com>
2024-12-06 15:59:54 -0300

    tipos_dados_numbers

---
+++ src/js/script05.js
@@ -0,0 +1,38 @@
+var num1 = 20;
+var num2 = 3;
+
+var soma = num1 + num2;
+console.log("Soma: " + num1 + " + " + num2 + " = " + soma);
+
+var subtracao = num1 - num2;
+console.log("Subtração: " + num1 + " - " + num2 + " = " + subtracao);
+
+var multiplicacao = num1 * num2;
+console.log("Multiplicação: " + num1 + " * " + num2 + " = " + multiplicacao);
+
+var divisao = num1 / num2;
+console.log("Divisão: " + num1 + " / " + num2 + " = " + divisao);
+
+var media = (num1 + num2) / 2;
+console.log("Média: (" + num1 + " + " + num2 + ") / 2 = " + media);
+
+console.log("--------------------------------------------------");
+console.log(" ");
+
+var potencia = Math.pow(2, 4);
+console.log("Potência: Math.pow(2, 4) = " + potencia); // Potência: Math.pow(2, 4) = 16
+
+var arredonda = Math.round(2.7);
+console.log("Arredondamento: Math.round(2.7) = " + arredonda); // Arredondamento: Math.round(2.7) = 3
+
+var arredonda_cima = Math.ceil(4.3);
+console.log("Arredondamento para cima: Math.ceil(4.3) = " + arredonda_cima); // Arredondamento para cima: Math.ceil(4.3) = 5
+
+var arredonda_baixo = Math.floor(4.6);
+console.log("Arredondamento para baixo: Math.floor(4.6) = " + arredonda_baixo); // Arredondamento para baixo: Math.floor(4.6) = 4
+
+var raiz = Math.sqrt(36);
+console.log("Raiz quadrada: Math.sqrt(36) = " + raiz); // Raiz quadrada: Math.sqrt(36) = 6
+
+console.log("--------------------------------------------------");
+console.log(" ");
